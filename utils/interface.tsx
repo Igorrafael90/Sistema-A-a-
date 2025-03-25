1fb3fff363e697314c1a5a33e64832804cd0e82a
Igor Rafael Eloi dos Santos <Igor.Eloi@fies.org.br>
2025-03-25 08:18:55 -0300

    Interface para pedidos

--- utils/interface.tsx
+++ utils/interface.tsx
@@ -2,3 +2,10 @@ export interface Cadastrado {
     user: string,
     password: string,
 }
+
+export interface ORDERS {
+    id: number,
+    order: string,
+    amount: number,
+    local: string,
+}
